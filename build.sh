e9d844b18a47d7d067e0d99df8dc090ddfad32db
Rams-Git1 <112711258+Rams-Git1@users.noreply.github.com>
2023-01-16 11:44:24 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#The script to build docker image
+
+export TYPE=$1
+export TAG=0.$2
+export PATH=/usr/libexec/docker/cli-plugins:$PATH
+docker-compose -f docker-compose.yml build
+if [[ $? -eq 0 ]]
+then
+echo "docker build by docker-compose is successful"
+exit 0
+else
+echo "docker build failed"
+exit 1
+fi
